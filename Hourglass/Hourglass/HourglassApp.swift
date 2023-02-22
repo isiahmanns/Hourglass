a7d29d0c80b353446bfa9f3e336f8f3e70756f7b
Isiah Manns <isiah.michael1018@gmail.com>
2023-02-22 00:07:12 -0500

    Boilerplate app code

---
+++ Hourglass/Hourglass/HourglassApp.swift
@@ -0,0 +1,17 @@
+//
+//  HourglassApp.swift
+//  Hourglass
+//
+//  Created by Isiah Manns on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct HourglassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
