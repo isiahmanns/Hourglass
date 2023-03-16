f8a87d6064fd2a7a6164038fb7f55d1af7e9d42e
Isiah Manns <isiah.michael1018@gmail.com>
2023-03-16 01:21:09 -0400

    Modify UI padding

--- Hourglass/Hourglass/ContentView.swift
+++ Hourglass/Hourglass/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
 
                 SettingsButton()
             }
-            .padding(40)
+            .padding([.top, .bottom], 40)
+            .padding([.leading, .trailing], 60)
             .background(Color.background)
         }
     }
