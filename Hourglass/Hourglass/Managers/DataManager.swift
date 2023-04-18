3208d67c68d02f6de9653a3c027e9e1785a03eec
Isiah Manns <isiah.michael1018@gmail.com>
2023-04-18 02:04:44 -0400

    Convert timer models data to dictionary
    - Allows quick lookup via ID when receiving timer events

--- Hourglass/Hourglass/Managers/DataManager.swift
+++ Hourglass/Hourglass/Managers/DataManager.swift
@@ -1,5 +1,5 @@
 protocol DataManaging {
-    func loadTimerModels() -> [Timer.Model]
+    func loadTimerModels() -> [Timer.Model.ID: Timer.Model]
 }
 
 struct DataManager: DataManaging {
@@ -10,33 +10,37 @@ struct DataManager: DataManaging {
         self.settingsManager = settingsManager
     }
 
-    func loadTimerModels() -> [Timer.Model] {
-        [Timer.Model(length: settingsManager.getTimerLength(for: .timerFocusSmall),
-                     category: .focus,
-                     size: .small),
-         Timer.Model(length: settingsManager.getTimerLength(for: .timerFocusMedium),
-                     category: .focus,
-                     size: .medium),
-         Timer.Model(length: settingsManager.getTimerLength(for: .timerFocusLarge),
-                     category: .focus,
-                     size: .large),
-         Timer.Model(length: settingsManager.getTimerLength(for: .timerRestSmall),
-                     category: .rest,
-                     size: .small),
-         Timer.Model(length: settingsManager.getTimerLength(for: .timerRestMedium),
-                     category: .rest,
-                     size: .medium),
-         Timer.Model(length: settingsManager.getTimerLength(for: .timerRestLarge),
-                     category: .rest,
-                     size: .large)]
+    func loadTimerModels() -> [Timer.Model.ID: Timer.Model] {
+        let timerModels = [
+            Timer.Model(length: settingsManager.getTimerLength(for: .timerFocusSmall),
+                        category: .focus,
+                        size: .small),
+            Timer.Model(length: settingsManager.getTimerLength(for: .timerFocusMedium),
+                        category: .focus,
+                        size: .medium),
+            Timer.Model(length: settingsManager.getTimerLength(for: .timerFocusLarge),
+                        category: .focus,
+                        size: .large),
+            Timer.Model(length: settingsManager.getTimerLength(for: .timerRestSmall),
+                        category: .rest,
+                        size: .small),
+            Timer.Model(length: settingsManager.getTimerLength(for: .timerRestMedium),
+                        category: .rest,
+                        size: .medium),
+            Timer.Model(length: settingsManager.getTimerLength(for: .timerRestLarge),
+                        category: .rest,
+                        size: .large)
+        ]
+
+        return Dictionary(uniqueKeysWithValues: timerModels.map { ($0.id, $0) })
     }
 }
 
 struct DataManagerMock: DataManaging {
     let timerModels: [Timer.Model]
 
-    func loadTimerModels() -> [Timer.Model] {
-        return timerModels
+    func loadTimerModels() -> [Timer.Model.ID: Timer.Model] {
+        return Dictionary(uniqueKeysWithValues: timerModels.map { ($0.id, $0) })
     }
 }
 
--- Hourglass/HourglassTests/TimerManagerTests.swift
+++ Hourglass/HourglassTests/TimerManagerTests.swift
@@ -4,7 +4,7 @@ import XCTest
 final class TimerManagerTests: XCTestCase {
 
     let (timerPublisher, timerManager) = UnitTestProviders.fakeTimerManager
-    let timerID = UUID()
+    let timerID = ObjectIdentifier(Timer.Model)
     let now = Date.now
 
     override func setUpWithError() throws {
