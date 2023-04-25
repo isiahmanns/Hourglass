1fed3f4ad3001a00591bc154120884c305f49352
Isiah Manns <isiah.michael1018@gmail.com>
2023-04-24 21:14:01 -0400

    Assert for event triggers in TimerManagerTests

--- Hourglass/HourglassTests/TimerManagerTests.swift
+++ Hourglass/HourglassTests/TimerManagerTests.swift
@@ -1,3 +1,4 @@
+import Combine
 import XCTest
 @testable import Hourglass
 
@@ -6,8 +7,36 @@ final class TimerManagerTests: XCTestCase {
     let (timerPublisher, timerManager) = UnitTestProviders.fakeTimerManager
     let timerID = ObjectIdentifier(Timer.Model)
     let now = Date.now
+    var cancellables: Set<AnyCancellable> = []
+    var eventTriggerCount = (timerDidStart: 0,
+                             timerDidTick: 0,
+                             timerDidComplete: 0,
+                             timerWasCancelled: 0)
 
     override func setUpWithError() throws {
+        timerManager.events[.timerDidStart]?
+            .sink { [weak self] timerModelID in
+                self?.eventTriggerCount.timerDidStart += 1
+            }
+            .store(in: &cancellables)
+
+        timerManager.events[.timerDidTick]?
+            .sink { [weak self] timerModelID in
+                self?.eventTriggerCount.timerDidTick += 1
+            }
+            .store(in: &cancellables)
+
+        timerManager.events[.timerDidComplete]?
+            .sink { [weak self] timerModelID in
+                self?.eventTriggerCount.timerDidComplete += 1
+            }
+            .store(in: &cancellables)
+
+        timerManager.events[.timerWasCancelled]?
+            .sink { [weak self] timerModelID in
+                self?.eventTriggerCount.timerWasCancelled += 1
+            }
+            .store(in: &cancellables)
     }
 
     override func tearDownWithError() throws {
@@ -18,19 +47,26 @@ final class TimerManagerTests: XCTestCase {
 
         timerManager.startTimer(length: 3, activeTimerModelId: timerID)
         XCTAssertEqual(timerManager.timeStamp, "00:03")
+        XCTAssertEqual(eventTriggerCount.timerDidStart, 1)
         assertTimerInProgress()
 
         timerPublisher.send(now)
         XCTAssertEqual(timerManager.timeStamp, "00:02")
+        XCTAssertEqual(eventTriggerCount.timerDidTick, 1)
         assertTimerInProgress()
 
         timerPublisher.send(now + 1)
         XCTAssertEqual(timerManager.timeStamp, "00:01")
+        XCTAssertEqual(eventTriggerCount.timerDidTick, 2)
         assertTimerInProgress()
 
         timerPublisher.send(now + 2)
         XCTAssertEqual(timerManager.timeStamp, "00:00")
+        XCTAssertEqual(eventTriggerCount.timerDidTick, 3)
+        XCTAssertEqual(eventTriggerCount.timerDidComplete, 1)
         assertTimerDefault()
+
+        XCTAssertEqual(eventTriggerCount.timerWasCancelled, 0)
     }
 
     func testTimerCountdownCancel() {
@@ -38,11 +74,16 @@ final class TimerManagerTests: XCTestCase {
 
         timerManager.startTimer(length: 3, activeTimerModelId: timerID)
         XCTAssertEqual(timerManager.timeStamp, "00:03")
+        XCTAssertEqual(eventTriggerCount.timerDidStart, 1)
         assertTimerInProgress()
 
         timerManager.cancelTimer()
         XCTAssertEqual(timerManager.timeStamp, "00:00")
+        XCTAssertEqual(eventTriggerCount.timerWasCancelled, 1)
         assertTimerDefault()
+
+        XCTAssertEqual(eventTriggerCount.timerDidTick, 0)
+        XCTAssertEqual(eventTriggerCount.timerDidComplete, 0)
     }
 
     private func assertTimerInProgress() {
